5f44faaab797852d48d2fddbed434e2879a13574
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-11 17:46:38 +0000

    implementing movement

--- input.js
+++ input.js
@@ -1,11 +1,22 @@
+let connection;
+
 const handleUserInput = (data) => {
   if(data === "\x03") {
     console.log("Exiting");
     process.exit();
-  };
+  } else if (data === "w") {
+    connection.write('Move: up');
+  } else if (data === "a") {
+    connection.write('Move: left');
+  } else if (data === "s") {
+    connection.write('Move: down'); 
+  } else if (data === "d") {
+    connection.write('Move: right');
+  }
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
