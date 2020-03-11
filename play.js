bced13569ca130f7f8c887b33ca913621e689920
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-11 16:31:58 +0000

    refactoring

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require('net');
-const stdin = process.stdin;
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  // handles incoming data and prints it
-  conn.on('data', (data) => {
-    console.log(data);
-  })
-
-  return conn;
-}
+const {connect} = require('./client');
 
 console.log('Connecting ...');
 connect();
