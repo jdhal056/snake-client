6ba0afa8e03f9db28892141a89fd3f7227fe6f91
Jaspreet Dhaliwal <jdhal056@gmail.com>
2020-03-11 17:13:00 +0000

    handling ctrl c to terminate

--- play.js
+++ play.js
@@ -2,3 +2,21 @@ const {connect} = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = (data) => {
+  if(data === "\x03") {
+    process.exit();
+  };
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+
+setupInput();
